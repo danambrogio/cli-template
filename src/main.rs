f488184c6cc0e5a2808204b04bba5b397933c042
Dan Ambrogio <dan.ambrogio@gmail.com>
2020-01-24 12:19:40 -0500

    switches to 2-space indentation because I like it.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ extern crate clap;
 use clap::App;
 
 fn main() {
-    let yaml = load_yaml!("../cli.yml");
-    let _matches = App::from_yaml(yaml).get_matches();
+  let yaml = load_yaml!("../cli.yml");
+  let _matches = App::from_yaml(yaml).get_matches();
 }
